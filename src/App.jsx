05263363d18ed37333842c3fd704c6f2011c7f42
Roberto Ramirez <robert.624@hotmail.com>
2023-05-31 17:09:22 -0500

    project stats component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './Layout/Header'
 import './App.scss'
 import ProjectTitle from './components/ProjectTitle'
+import ProjectStats from './components/ProjectStats'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <main>
         <ProjectTitle/>
+        <ProjectStats/>
       </main>
     </>
   )
---
+++ src/components/ProjectStats.jsx
@@ -0,0 +1,27 @@
+import './project-stats.scss'
+
+const ProjectStats = () => {
+  return (
+    <div className='project-stats'>
+        <div className="stat">
+            <div className="value">$89,914</div>
+            <div className="type">of $100,000 backed</div>
+        </div>
+        <div className="stat">
+            <div className="value">5,007</div>
+            <div className="type">total backers</div>
+        </div>
+        <div className="stat">
+            <div className="value">56</div>
+            <div className="type">days left</div>
+        </div>
+        <div className="progress">
+                <div className="progress-bar">
+                    <div className="progress-bar-fill"></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectStats
