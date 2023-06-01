2531d30fe44adcf042e6f1a7bdcf988797ff74a5
Roberto Ramirez <robert.624@hotmail.com>
2023-06-01 11:31:16 -0500

    mobile menu added

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,13 +1,28 @@
+import { useState } from 'react'
 import "./header.scss";
 
 const Header = () => {
+
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <header>
             <div className="inner">
                 <a href="/">
                     <img src="/logo.svg" alt="crowdfund logo" className="logo" />
                 </a>
-                <img src="/icon-hamburger.svg" alt="" className="hamburger" />
+                <img onClick={toggleMobileMenu} src={`/${isMobileMenuOpen ? 'icon-close-menu.svg':'icon-hamburger.svg'}`} alt="" className="menu-toggler" />
+                <dialog open={isMobileMenuOpen} className="mobile-menu">
+                    <nav>
+                        <a href="/">About</a>
+                        <a href="/">Discover</a>
+                        <a href="/">Get Started</a>
+                    </nav>
+                </dialog>
             </div>
         </header>
     );
